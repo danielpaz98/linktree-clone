dd946ca661eddf67d53949e14253352b388c84a4
Daniel Paz <alexdaniel-1998@outlook.com>
2021-07-02 04:13:45 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Home from "~/views/Home";
+
+function App() {
+	return (
+		<main className="App bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-50">
+			<Home />
+		</main>
+	);
+}
+
+export default App;
