06e28f0acdadceadcdd79837f7c6ca00f6d51d2f
Daniel Paz <alexdaniel-1998@outlook.com>
2021-07-03 09:34:38 -0500

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,9 +7,9 @@ import user from "~/fakeAPI/user";
 
 export default function Home() {
 	return (
-		<div>
+		<>
 			<Card {...user} />
 			<Footer icon="icon-linktree" text="linktree" />
-		</div>
+		</>
 	);
 }
