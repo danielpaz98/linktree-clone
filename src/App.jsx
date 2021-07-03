bd4fdcfd783d6efbd32daffb4fa2b79cef2c1d22
Daniel Alexander Paz Rodríguez <alexdaniel-1998@outlook.com>
2021-07-03 09:31:24 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "~/views/Home";
 
 function App() {
 	return (
-		<main className="App bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-50">
+		<main>
 			<Home />
 		</main>
 	);
